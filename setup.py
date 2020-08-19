6742c9b45ea95ac70c6aef349174c8622d987d24
johannesgef <65153768+johannesgef@users.noreply.github.com>
2020-08-19 16:33:07 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='drivendata.org Competition: Flu Shot Learning (https://www.drivendata.org/competitions/66/flu-shot-learning/)',
+    author='christoph',
+    license='',
+)
